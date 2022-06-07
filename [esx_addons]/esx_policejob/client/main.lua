7d29ad4bf23722b18e8bd0b9cc8cc4e0b0b56f3c
Dev Capy <78591839+mr-capy@users.noreply.github.com>
2022-06-08 00:03:06 +0500

    feat(esx_policejob): adding short key for handcuff

--- [esx_addons]/esx_policejob/client/main.lua
+++ [esx_addons]/esx_policejob/client/main.lua
@@ -1571,6 +1571,22 @@ function StartHandcuffTimer()
 	end)
 end
 
+if Config.EnableCuffCommand then
+	RegisterCommand(Config.CuffCommand, function(src)
+		if ESX.PlayerData.job and ESX.PlayerData.job.name == 'police' and not ESX.PlayerData.dead then
+			local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+			if closestPlayer ~= -1 and closestDistance <= 3.0 then
+				TriggerServerEvent('esx_policejob:handcuff', GetPlayerServerId(closestPlayer))
+			else
+				ESX.ShowNotification(_U('no_players_nearby') , "info" , 5000) 
+			end
+		end
+	end)
+end
+if Config.EnableCuffKey then
+RegisterKeyMapping(Config.CuffCommand, "Police Cuff Key", "keyboard", Config.CuffKey)
+end
+
 -- TODO
 --   - return to garage if owned
 --   - message owner that his vehicle has been impounded
